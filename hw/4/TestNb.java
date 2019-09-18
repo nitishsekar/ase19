7f232d3671a5e404114c7c30b689d230f1295a72
Nitish Sekar <47374290+nitishsekar@users.noreply.github.com>
2019-09-18 17:17:44 -0400

    Update TestNb.java

--- hw/4/TestNb.java
+++ hw/4/TestNb.java
@@ -4,12 +4,12 @@ public class TestNb {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Nb nb  = new Nb();
-		nb.readFileAndClassify("C:\\Users\\nitis\\Desktop\\CSC 591 - ASE\\HW 4\\weathernon.csv");
+		nb.readFileAndClassify("weathernon.csv");
 		nb.displayResults();
 		System.out.println();
 		
 		Nb nb2 = new Nb();
-		nb2.readFileAndClassify("C:\\Users\\nitis\\Desktop\\CSC 591 - ASE\\HW 4\\diabetes.csv");
+		nb2.readFileAndClassify("diabetes.csv");
 		nb2.displayResults();
 		
 	}
