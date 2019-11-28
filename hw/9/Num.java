5e77e2db9947e9982272d4fdc3cff85fb9641022
Nitish Sekar <47374290+nitishsekar@users.noreply.github.com>
2019-11-28 17:04:45 -0500

    Similarity tests added to Num: t-Test and hedges

--- hw/9/Num.java
+++ hw/9/Num.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
@@ -14,6 +15,7 @@ public class Num extends Col implements Cloneable {
     private Double stdDev;
     private Float hi;
     private Float low;
+    
     
     public Num() {
     	super();
@@ -209,7 +211,91 @@ public class Num extends Col implements Cloneable {
 	public Float norm(Float x, Num n) {
 		return (float) ((x-n.getLow())/(n.getHi() - n.getLow() + Math.pow(10, -32)));
 	}
+	
+	/* Similarity tests - For "Same" score */
+	public boolean same(Num num1, Num num2, Double conf, Double small) {
+		return tTestSame(num1, num2, conf) || hedges(num1, num2, small);
+	}
+	
+	public boolean tTestSame(Num num1, Num num2, Double conf) {
+		Double nom = Math.abs(num1.getMean() - num2.getMean());
+		Double s1 = num1.getStdDev();
+		Double s2 = num2.getStdDev();
+		Double denom;
+		if(s1 > 0 && s2 > 0) {
+			denom = Math.sqrt((s1/num1.getCount()) + (s2/num2.getCount()));
+		} else {
+			denom = 1.0;
+		}
+		int df = Math.min(num1.getCount() - 1, num2.getCount() - 1);
+		return criticalValue(df, conf) >= nom/denom;
+	}
 	 
+	public Double criticalValue(int df, Double conf) {
+		int[] xsArr = new int[]{ 1,2,5,10,15,20,25,30,60,100 }; 
+		List<Integer> xs = new ArrayList<>();
+		for(int i=0; i<10; i++) {
+			xs.add(xsArr[i]);
+		}
+		HashMap<Double, List<Double>> cvMap = new HashMap<>();
+		double[] ysArr1 = new double[]{ 3.078, 1.886, 1.476, 1.372, 1.341, 1.325, 1.316, 1.31,  1.296, 1.29 }; 
+		List<Double> ys1 = new ArrayList<>();
+		for(int i=0; i<10; i++) {
+			ys1.add(ysArr1[i]);
+		}
+		double[] ysArr2 = new double[]{ 6.314, 2.92,  2.015, 1.812, 1.753, 1.725, 1.708, 1.697, 1.671, 1.66 }; 
+		List<Double> ys2 = new ArrayList<>();
+		for(int i=0; i<10; i++) {
+			ys2.add(ysArr2[i]);
+		}
+		double[] ysArr3 = new double[]{ 31.821, 6.965, 3.365, 2.764, 2.602, 2.528, 2.485, 2.457, 2.39,  2.364 }; 
+		List<Double> ys3 = new ArrayList<>();
+		for(int i=0; i<10; i++) {
+			ys3.add(ysArr3[i]);
+		}
+		cvMap.put(0.9,ys1);
+		cvMap.put(0.95,ys2);
+		cvMap.put(0.99,ys3);
+		return interpolate(df, xs, cvMap.get(conf));
+	}
+	
+	public Double interpolate(int df, List<Integer> xs, List<Double> cvVals) {
+		if(df <= xs.get(0)) 
+			return cvVals.get(0);
+		if(df >= xs.get(xs.size()-1)) 
+			return cvVals.get(xs.size()-1);
+		int x0 = xs.get(0);
+		Double y0 = cvVals.get(0);
+		int x1 = x0; Double y1 = y0;
+		for(int i = 1; i < xs.size(); i++) {
+			x1 = xs.get(i);
+			y1 = cvVals.get(i);
+			if((df < x0) || (df > xs.get(xs.size()-1)) || ((x0 <= df) && (df < x1))) {
+				break;
+			}
+			x0 = x1;
+			y0 = y1;
+		}
+		int gap = (df-x0)/(x1-x0);
+		return y0 + gap*(y1-y0);
+	}
+	
+	public boolean hedges(Num num1, Num num2, Double small) {
+		
+		/*
+	    Hedges effect size test.
+	    Returns true if the "i" and "j" difference is only a small effect.
+	    "i" and "j" are   objects reporing mean (i.mu), standard deviation (i.s) 
+	    and size (i.n) of two  population of numbers.
+	    */
+		Double num   = (num1.getCount() - 1)*Math.pow(num1.getStdDev(),2) + (num2.getCount() - 1)*Math.pow(num2.getStdDev(),2);
+	    int denom = (num1.getCount() - 1) + (num2.getCount() - 1);
+	    Double sp = Math.sqrt(num/denom);
+	    Double delta = Math.abs(num1.getMean() - num2.getMean()) / sp;  
+	    Double c = 1 - 3.0 / (4*((num1.getCount() - 1) + (num1.getCount() - 1)) - 1);
+	    return delta * c < small;
+	}
+	
 	private void setLow() {
 		float min = Float.MAX_VALUE;
 		for(float f: valList) {
